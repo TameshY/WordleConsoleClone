10c4afe195f66c81178e113cde4d20677f2e0c43
TameshY <140468251+TameshY@users.noreply.github.com>
2023-07-26 11:56:25 -0400

    Built game outline, Need Logic

---
+++ WordleConsoleClone/Alphabet.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace WordleConsoleClone
+{
+	public class Alphabet
+	{
+        public string builder(string word)
+        {
+            string char1, char2, char3, char4, char5;
+            char[] letterArray = word.ToArray();
+            char1 = letterArray[0].ToString();
+            char2 = letterArray[1].ToString();
+            char3 = letterArray[2].ToString();
+            char4 = letterArray[3].ToString();
+            char5 = letterArray[4].ToString();
+
+
+            String returnStr = 
+            "┌───┐ ┌───┐ ┌───┐ ┌───┐ ┌───┐\n" +
+            "│ " + char1 + " │ │ " + char2 + " │ │ " + char3 + " │ │ " + char4 + " │ │ " + char5 + " │\n" +
+            "└───┘ └───┘ └───┘ └───┘ └───┘";
+
+            return returnStr;
+        }
+
+
+    }
+}
+
--- WordleConsoleClone/Program.cs
+++ WordleConsoleClone/Program.cs
@@ -1,3 +1,51 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 
+using WordleConsoleClone;
+using System.Collections.Generic;
+
+Random rand = new Random();
+
+int randWordIndex = rand.Next(1, 14856);
+
+//Converts to array
+string[] words = File.ReadAllLines(@"/Users/tamesh/Projects/WordleConsoleClone/WordleConsoleClone/valid-wordle-words.txt");
+
+
+//Gets random word from wordle list
+string answer = words[randWordIndex];
+char[] answerArray = answer.ToCharArray();
+
+//holds the formatting method for strings
+Alphabet stringFormatter = new Alphabet();
+
+//Initial Display
+Console.WriteLine("┌───┐ ┌───┐ ┌───┐ ┌───┐ ┌───┐");
+Console.WriteLine("│   │ │   │ │   │ │   │ │   │");
+Console.WriteLine("└───┘ └───┘ └───┘ └───┘ └───┘");
+
+
+
+
+bool guessing = true;
+
+while (guessing == true) {
+    Console.WriteLine("Enter a Guess: ");
+    string? guess = Console.ReadLine();
+
+    if (words.Contains(guess.ToLower()))
+    {
+        char[] guessLetterArray = guess.ToCharArray();
+        foreach (char letter in guessLetterArray)
+        {
+            if(answerArray.Contains(letter))
+            {
+
+            }
+        }
+    }
+    else
+    {
+        Console.WriteLine("Pick a different word");
+    }
+    //NEXT PRINT THE CHARCTER IN FACY FORMAT
+}
